c50c68463fcab22b11f71ac6fbccdd51a4bf6795
Ryan Scott <ryankennethscott@gmail.com>
2016-09-25 17:33:23 +1300

    Fixing some tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,17 +8,24 @@ describe Helpers do
      @schema = JSON.parse(File.read('./event_schema.json'))
    end
   
-     it "returns a returns true for an event with valid JSON schema" do
-         expect(Helpers.validate_JSON(@schema, JSON.parse(File.read('./spec/good_event.json')) == true))
+     it "returns true for an event with valid JSON schema" do
+         expect(Helpers.validate_JSON(JSON.parse(File.read('./spec/good_event.json')) == true))
     end
     
-    it "returns a returns false for an event with invalid JSON schema" do
-         expect(Helpers.validate_JSON(@schema, JSON.parse(File.read('./spec/bad_event.json')) == true))
+    it "returns false for an event with invalid JSON schema" do
+         expect(Helpers.validate_JSON(JSON.parse(File.read('./spec/bad_event.json')) == true))
     end
 
-    # it "returns nil when malformed JSON is provided" fo 
-    #   expect(Helpers.parse_request())
+    # it "returns nil when malformed JSON is provided" do
+    #   expect(Helpers.parse_request(File.read('./spec/broken_event.json')).nil? == true)
+    #  end
 
+    it "returns false when unsuccessfully pushing to ES" do
+      expect(Helpers.validate_JSON(JSON.parse(File.read('./spec/good_event.json')) == false))
+   end
+
+    # it "returns false when unsuccessfully pushing to ES" do
+    #   expect(Helpers.validate_JSON(JSON.parse(File.read('./spec/good_event.json')) == false))
     # end
 
   end
